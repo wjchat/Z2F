fb3b02651ac947737936b643bc4f8088c0b77afa
William Chatterson <wchatterson@ibm.com@Williams-MacBook-Pro.local>
2020-03-21 18:53:30 -0500

    Updates

---
+++ precache-manifest.37a74d04a35282d644281cb010e7e292.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e079ed27dbbe29fbc531ad4d80d823",
+    "url": "/Z2F/index.html"
+  },
+  {
+    "revision": "85ebfc9e78ba45f7d4da",
+    "url": "/Z2F/static/css/main.2abc2c2c.chunk.css"
+  },
+  {
+    "revision": "037792c0bbce5825591f",
+    "url": "/Z2F/static/js/2.954738d5.chunk.js"
+  },
+  {
+    "revision": "0bdb97d421f97884a5b69362f0009415",
+    "url": "/Z2F/static/js/2.954738d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ebfc9e78ba45f7d4da",
+    "url": "/Z2F/static/js/main.7af2afa3.chunk.js"
+  },
+  {
+    "revision": "077c4938f78732a77f31",
+    "url": "/Z2F/static/js/runtime-main.ec7a14db.js"
+  },
+  {
+    "revision": "f424730076d8b1fd09dddc237c5a39ee",
+    "url": "/Z2F/static/media/China.f4247300.png"
+  },
+  {
+    "revision": "d7aca009afebe84e94f1c37c8a08b2c7",
+    "url": "/Z2F/static/media/NewYork.d7aca009.png"
+  },
+  {
+    "revision": "705ce4e887112d502ff8ce190683f779",
+    "url": "/Z2F/static/media/Resources.705ce4e8.png"
+  },
+  {
+    "revision": "0847adff5a533e1b05c3d797d23e6847",
+    "url": "/Z2F/static/media/Shirt.0847adff.png"
+  },
+  {
+    "revision": "d2970ac508d5310dc66e524551299492",
+    "url": "/Z2F/static/media/Uzbek.d2970ac5.png"
+  },
+  {
+    "revision": "90f1a2fd6db67e8ee675e8d60d5a3e26",
+    "url": "/Z2F/static/media/arrow.90f1a2fd.svg"
+  },
+  {
+    "revision": "bc2ea15fe81a9d48d69af8b37e0cb6d9",
+    "url": "/Z2F/static/media/barn.bc2ea15f.svg"
+  },
+  {
+    "revision": "c55932876109cd883ce62165d2dac7c3",
+    "url": "/Z2F/static/media/factory.c5593287.svg"
+  },
+  {
+    "revision": "96e48f5518b64798384de1857f3faf66",
+    "url": "/Z2F/static/media/shop.96e48f55.svg"
+  }
+]);
